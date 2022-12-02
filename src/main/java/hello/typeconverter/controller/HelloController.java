83bd9e8df1f847f528971d0885bdee880a95b540
kk210001 <91827665+kk210001@users.noreply.github.com>
2022-12-02 19:32:42 +0900

    spring 이 제공하는 WebMvcConfigurer 의 addFormatters 으로 컨버터 등록 가능
    작동과정
    @RequestParam 이 있으면 ArgumentResolver 인 RequestParameterMethodArgumentResolver 에서 ConversionService를 사용해서 타입을 변환한다

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,6 +1,7 @@
 package hello.typeconverter.controller;
 
 
+import hello.typeconverter.type.IpPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,12 @@ public class HelloController {
     public String helloV2(@RequestParam Integer data) {
         System.out.println("data = " + data);
         return "v2 ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        System.out.println("ip = " + ipPort.getIp());
+        System.out.println("port = " + ipPort.getPort());
+        return "ok";
     }
 }
